eb31db54bd871bd6df1c91e1bc8e30fbba975c52
Norah.Waswala <norah.waswala@student.moringa.com>
2022-06-29 13:45:04 +0300

    feat: install virtual environment

--- quiz_app/views.py
+++ quiz_app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render('', 'index.html')
+
+def landing(request):
+    return render('', 'landing.html')
