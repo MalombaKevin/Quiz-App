541bf4c0717f23b8247680b9857d06f3410c9464
Michael Ogaye <hogaeymic@gmail.com>
2022-06-30 12:21:47 +0300

    class category

--- quiz_app/models.py
+++ quiz_app/models.py
@@ -2,12 +2,21 @@ from django.db import models
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
 from django.template.defaultfilters import slugify
+from cloudinary.models import CloudinaryField
+from django.conf import settings
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
 
 
 class Quiz(models.Model):
 	name = models.CharField(max_length=100)
 	description = models.CharField(max_length=70)
-	image = models.ImageField()
+	image = CloudinaryField('images')
 	slug = models.SlugField(blank=True)
 	roll_out = models.BooleanField(default=False)
 	timestamp = models.DateTimeField(auto_now_add=True)
