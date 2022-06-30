d9a9fb3314e1c542ba229702b88c0f381be5154d
Michael Ogaye <hogaeymic@gmail.com>
2022-06-30 12:31:03 +0300

    ft:answer model and the relationships

--- quiz_app/models.py
+++ quiz_app/models.py
@@ -36,4 +36,11 @@ class Question(models.Model):
 	def __str__(self):
 		return self.label
 
+class Answer(models.Model):
+	question = models.ForeignKey(Question, on_delete=models.CASCADE)
+	label = models.CharField(max_length=100)
+	is_correct = models.BooleanField(default=False)
+
+	def __str__(self):
+		return self.label
 
