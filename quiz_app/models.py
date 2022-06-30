e073d2adce0b5ed4f5c92bc35ac9b1c6cc23cfe5
Michael Ogaye <hogaeymic@gmail.com>
2022-06-30 12:17:29 +0300

    feat: Quiz model

--- quiz_app/models.py
+++ quiz_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from django.template.defaultfilters import slugify
 
-# Create your models here.
+
+class Quiz(models.Model):
+	name = models.CharField(max_length=100)
+	description = models.CharField(max_length=70)
+	image = models.ImageField()
+	slug = models.SlugField(blank=True)
+	roll_out = models.BooleanField(default=False)
+	timestamp = models.DateTimeField(auto_now_add=True)
+
+	class Meta:
+		ordering = ['timestamp',]
+		verbose_name_plural = "Quizzes"
+
+	def __str__(self):
+		return self.name
+
+
+
