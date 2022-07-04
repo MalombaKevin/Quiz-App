3d8a79781cdf5b1cd04c10396e2cfa3a57457ea6
Michael Ogaye <hogaeymic@gmail.com>
2022-07-04 12:41:56 +0300

    ft:quizlist serializer and quiz answers serializers

--- quiz_app/serializers.py
+++ quiz_app/serializers.py
@@ -1,4 +1,4 @@
-from .models import Category, QuizTaker, Question, Answer, UsersAnswer
+from .models import Category, QuizTaker, Question, Answer, UsersAnswer,Quiz
 from rest_framework import serializers
 
 
@@ -7,6 +7,18 @@ class CategorySerializer(serializers.ModelSerializer):
         model = Category
         fields = '__all__'
         read_only_fields = ('__all__',)
+
+
+class QuizListSerializer(serializers.ModelSerializer):
+	questions_count = serializers.SerializerMethodField()
+	class Meta:
+		model = Quiz
+		fields = ["id", "name", "description", "image", "slug", "questions_count"]
+		read_only_fields = ["questions_count"]
+
+	def get_questions_count(self, obj):
+		return obj.question_set.all().count()
+
 
 
 class AnswerSerializer(serializers.ModelSerializer):
@@ -36,3 +48,21 @@ class QuizTakerSerializer(serializers.ModelSerializer):
 		model = QuizTaker
 		fields = "__all__"
 
+
+
+class QuizResultSerializer(serializers.ModelSerializer):
+	quiztaker_set = serializers.SerializerMethodField()
+	question_set = QuestionSerializer(many=True)
+
+	class Meta:
+		model = Quiz
+		fields = "__all__"
+
+	def get_quiztaker_set(self, obj):
+		try:
+			quiztaker = QuizTaker.objects.get(user=self.context['request'].user, quiz=obj)
+			serializer = QuizTakerSerializer(quiztaker)
+			return serializer.data
+
+		except QuizTaker.DoesNotExist:
+			return None 
