608ba783e7b8c47e6227333c99c34b82e94986bf
Michael Ogaye <hogaeymic@gmail.com>
2022-07-04 13:19:07 +0300

    QuizlistApiview

--- quiz_app/urls.py
+++ quiz_app/urls.py
@@ -3,5 +3,4 @@ from .import views
 
 urlspattern = [
     path('', views.index, name='index'),
-    path('', views.landing, name='landing'),
 ]
--- quiz_app/views.py
+++ quiz_app/views.py
@@ -1,11 +1,52 @@
 from django.shortcuts import render
+from django.db.models import Q
+from django.shortcuts import get_object_or_404
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from .models import Answer, Question, Quiz, QuizTaker, UsersAnswer
+from .serializers import MyQuizListSerializer, QuizDetailSerializer, QuizListSerializer, QuizResultSerializer, UsersAnswerSerializer
+
 
 # Create your views here.
 def index(request):
     return render('', 'index.html')
 
-def landing(request):
-    return render('', 'landing.html')
-    
-def login(request):
-    return render('', 'login.html')
+#Api views
+
+
+class MyQuizListAPI(generics.ListAPIView):
+	permission_classes = [
+		permissions.IsAuthenticated
+	]
+	serializer_class = MyQuizListSerializer
+
+	def get_queryset(self, *args, **kwargs):
+		queryset = Quiz.objects.filter(quiztaker__user=self.request.user)
+		query = self.request.GET.get("q")
+
+		if query:
+			queryset = queryset.filter(
+				Q(name__icontains=query) |
+				Q(description__icontains=query)
+			).distinct()
+
+		return queryset
+
+        
+class QuizListAPI(generics.ListAPIView):
+	serializer_class = QuizListSerializer
+	permission_classes = [
+		permissions.IsAuthenticated
+	]
+
+	def get_queryset(self, *args, **kwargs):
+		queryset = Quiz.objects.filter(roll_out=True).exclude(quiztaker__user=self.request.user)
+		query = self.request.GET.get("q")
+
+		if query:
+			queryset = queryset.filter(
+				Q(name__icontains=query) |
+				Q(description__icontains=query)
+			).distinct()
+
+		return 
