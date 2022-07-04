b89d3398b4f635d8553163aa9f80857072afe189
Michael Ogaye <hogaeymic@gmail.com>
2022-07-04 14:15:34 +0300

    url patterns

--- quiz_app/views.py
+++ quiz_app/views.py
@@ -9,7 +9,7 @@ from .serializers import MyQuizListSerializer, QuizDetailSerializer, QuizListSer
 
 # Create your views here.
 def index(request):
-    return render('', 'index.html')
+    return render(request, 'index.html')
 
 #Api views
 
